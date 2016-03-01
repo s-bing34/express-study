e7d49029ed2d117165bf5f284ebd7ca77e29091d
s-bing34 <289860017@qq.com>
2016-03-01 16:14:26 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 module.exports = {
   mysql: {
-    host: '192.168.2.48', 
-    user: 'whales',
-    password: 'whales123',
+    host: '127.0.0.1', 
+    user: 'test',
+    password: 'test',
     database:'test_su', // 前面建的user表位于这个数据库中
     port: 3306
   }
